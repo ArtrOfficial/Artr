a4bd6317f9b1577b0559d69cb0cd7859eaedf789
Val <vwong031@ucr.edu>
2024-06-25 00:04:35 -0700

    basic initial sign up page layout. centered horizontally

--- artr/app/src/components/SignIn.js
+++ artr/app/src/components/SignIn.js
@@ -2,8 +2,42 @@ import React from 'react';
 
 function SignIn() {
   return (
-    <div className="bg-white">
-      sign in 
+    <div>
+      <div className="flex flex-col items-center">
+        <div>
+          Artr
+        </div>
+        <div className="flex flex-col">
+          <label>Username or Email</label>
+          <input type="text" className="border border-customPurple rounded-lg"/>
+        </div>
+        <div className="flex flex-col">
+          <div className='flex'>
+            <label>
+              Password
+            </label>
+            <div className="text-customPurple underline text-xs">
+              Forgot Password?
+            </div>
+          </div>
+          <input type="text" className="border border-customPurple rounded-lg"/>
+        </div>
+        <button className="bg-customPurple text-white rounded-xl w-20">
+          Login
+        </button>
+        <div>
+          Or
+        </div>
+        <div className="border border-customPurple rounded-lg">
+          Sign in with Google
+        </div>
+        <div>
+          New to Artr?
+        </div>
+        <button className='bg-customPurple text-white rounded-xl w-20'>
+          Sign Up
+        </button>
+      </div>
     </div>
   );
 }
