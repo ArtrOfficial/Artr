d2ee8f06bc02ac05728cf37ed0f1b86c776d62e1
Val <vwong031@ucr.edu>
2024-06-28 15:36:16 -0700

    step 1 sign up page almost done. trying to import footer

--- artr/app/pages/SignUp.js
+++ artr/app/pages/SignUp.js
@@ -3,24 +3,34 @@
 import React from 'react';
 import { useState } from 'react';
 import ProgressBar from '../src/components/ProgressBar';
+import Footer from '../src/components/Footer';
 
 function SignUp() {
   const [currentStep, setCurrentStep] = useState(1);
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center mt-24">
       <div className='font-sans text-base font-medium mb-6'>
         Create new Artr Account
       </div>
       <ProgressBar currentStep={currentStep} />
       <div className='flex font-sans text-medium font-medium text-center text-lg'>
-        <div className='bg-lilac w-52 h-52 rounded-xl mr-6'>
+        <div className='bg-lilac w-52 h-48 rounded-xl mr-6'>
           <div className='mt-2'>General Account</div>
         </div>
-        <div className='bg-lilac w-52 h-52 rounded-xl ml-6'>
-        <div className='mt-2'>Artist Account</div>
+        <div className='bg-lilac w-52 h-48 rounded-xl ml-6'>
+          <div className='mt-2'>Artist Account</div>
         </div>
       </div>
+      <div className='flex mt-10'>
+        <button className='underline text-customPurple text-sm mr-6'>
+          Cancel
+        </button>
+        <button className='text-sm bg-customPurple text-white rounded-xl w-16 h-6 ml-6'>
+          Next
+        </button>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ artr/app/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <img src='footer.png' alt="Artr footer. A big squiggly line throughout the bottom of the page with Artr information such as Support, Help, About, Artists, Community, Privacy, and Terms and Conditions." />
+  );
+}
+
+export default Footer;
