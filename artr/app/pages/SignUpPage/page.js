5c611bac74176f816cd8fcb87b5ee4821bcd6900
Val <vwong031@ucr.edu>
2024-07-02 19:27:51 -0700

    re-added footer to multi-page sign up

--- artr/app/pages/SignUpPage/page.js
+++ artr/app/pages/SignUpPage/page.js
@@ -1,8 +1,12 @@
 import React from "react";
 import SignUp from "./SignUp";
+import Footer from "@/app/components/Footer";
 
 export default function Page() {
   return(
-    <SignUp />
+    <>
+      <SignUp />
+      <Footer />
+    </>
   );
 }
